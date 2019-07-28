17acbb0567b95a1fe3128736669cf223258b8e58
Ludwig Fiolka <urushiraa@gmail.com>
2019-07-28 19:51:18 +0200

    [develop] Added buzzer integration

---
+++ devices/mbot-buzzer/lib/mbot-buzzer/src/buzzer.cpp
@@ -0,0 +1,31 @@
+#include <mbot/buzzer.h>
+
+using namespace mbot;
+
+bool buzzer_t::open(int port)
+{
+  _device.setpin(port);
+  return true;
+}
+
+void buzzer_t::play(uint16_t frequency, uint32_t ms, uint32_t wait_ms)
+{
+  _device.tone(frequency, ms);
+  if (wait_ms > 0)
+  {
+    delay(wait_ms);
+  }
+}
+
+void buzzer_t::play(const uint16_t* tones, size_t count, uint32_t ms, uint32_t wait_ms)
+{
+  for(size_t ii=0; ii < count; ++ii)
+  {
+    play(tones[ii], ms, wait_ms);
+  }
+}
+
+void buzzer_t::stop()
+{
+  _device.noTone();
+}
---
+++ devices/mbot-buzzer/lib/mbot-buzzer/src/mbot/buzzer.h
@@ -0,0 +1,47 @@
+#ifndef _MBOT_BUZZER_H_
+#define _MBOT_BUZZER_H_
+
+#include <MeBuzzer.h>
+
+namespace mbot
+{
+
+/**
+  * Wraps the buzzer connected to the auriga board.
+  */
+class buzzer_t
+{
+public:
+  buzzer_t() = default;
+  virtual ~buzzer_t() = default;
+
+  /**
+    * Opens the buzzer connected to the specified pin.
+    * By default, the buzzer of the Auriga board is located on the pin 45.
+    */
+  bool open(int port=45);
+
+  /**
+    * Plays a given tone during a given number of milliseconds and returns afterwards.
+    * If wait is > 0, this method only returns after a given number of milliseconds once the note has been played, allowing you to leave some time in between each tone.
+    */
+  void play(uint16_t frequency, uint32_t ms=UINT32_MAX, uint32_t wait_ms=0);
+
+  /**
+    * Play a series of tones in sequence, each of the given duration in ms and returns once all tones have been played.
+    * If wait is > 0, a given wait time in ms is inserted in between each tone.
+    */
+  void play(const uint16_t* tones, size_t count, uint32_t ms, uint32_t wait_ms=0);
+
+  /**
+    * Stops playing the current tone
+    */
+  void stop();
+
+private:
+  MeBuzzer _device;
+};
+
+} // namespace mbot
+
+#endif // _MBOT_BUZZER_H_
