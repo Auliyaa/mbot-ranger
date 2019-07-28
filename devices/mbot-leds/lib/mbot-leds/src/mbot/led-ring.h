dd7af9d9d1f35ab57a9e5d429e9ad9abd244501e
Ludwig Fiolka <urushiraa@gmail.com>
2019-07-28 18:01:33 +0200

    [develop] Added support for HSV

--- devices/mbot-leds/lib/mbot-leds/src/mbot/led-ring.h
+++ devices/mbot-leds/lib/mbot-leds/src/mbot/led-ring.h
@@ -13,7 +13,55 @@ namespace mbot
     MeRGBLed _device;
     uint8_t _led_count;
   public:
-    using rgb_t = cRGB;
+    struct hsv_t;
+
+    /**
+      * Describes a color in the RGB color space where:
+      * R (red) is in the range [0, 255]
+      * G (green) is in the range [0, 255]
+      * B (blue) is in the range [0, 255]
+      */
+    struct rgb_t
+    {
+      uint8_t r;
+      uint8_t g;
+      uint8_t b;
+
+      rgb_t(uint8_t r=0, uint8_t g=0, uint8_t b=0): r{r}, g{g}, b{b} {}
+      rgb_t(const cRGB& rgb): r{rgb.r}, g{rgb.g}, b{rgb.b} {}
+      rgb_t(const rgb_t& other) = default;
+
+      virtual ~rgb_t() = default;
+
+      /**
+        * Converts the current instance into the HSV colorspace
+        * See https://gist.github.com/fairlight1337/4935ae72bcbcc1ba5c72
+        */
+      hsv_t to_hsv() const;
+    };
+
+    /**
+      * Describes a color in the HSV Color space where:
+      * H (hue) is in the range [0,360]
+      * S (saturation) is in the range [0,100]
+      * V (value) is in the range [0,100]
+      */
+    struct hsv_t
+    {
+      uint16_t h;
+      uint8_t  s;
+      uint8_t  v;
+
+      hsv_t(uint16_t h=0, uint8_t s=0, uint8_t v=0): h{h}, s{s}, v{v} {}
+      hsv_t(const hsv_t& other) = default;
+      virtual ~hsv_t() = default;
+
+      /**
+        * Converts the current instance into the RGB color space.
+        * See https://gist.github.com/kuathadianto/200148f53616cbd226d993b400214a7f for details.
+        */
+      rgb_t to_rgb() const;
+    };
 
     /**
       * Constructor
@@ -46,15 +94,27 @@ namespace mbot
       * Sets the color for the LED at the specified index.
       * Note: You need to call the apply() method to apply all pending color changes.
       */
-    bool set_color(uint8_t idx, led_ring_t::rgb_t rgb);
+    bool set_color(uint8_t idx, const led_ring_t::rgb_t& rgb);
     bool set_color(uint8_t idx, uint8_t r, uint8_t g, uint8_t b);
+
+    /**
+      * Same as other set_color(idx) variants, but using the HSV colorspace.
+      * See led_strip_t::hsv_t for details.
+      */
+    bool set_color(uint8_t idx, const led_ring_t::hsv_t& hsv);
 
     /**
       * Sets the color for all LEDs.
       * Note: You need to call the apply() method to apply all pending color changes.
       */
-    bool set_color(led_ring_t::rgb_t rgb);
+    bool set_color(const led_ring_t::rgb_t& rgb);
     bool set_color(uint8_t r, uint8_t g, uint8_t b);
+
+    /**
+      * Same as other set_color() variants, but using the HSV colorspace.
+      * See led_strip_t::hsv_t for details.
+      */
+    bool set_color(const led_ring_t::hsv_t& hsv);
 
     /**
       * Sends all color changes applied through the set_color() methods calls since the last apply() call.
--- devices/mbot-leds/src/main.cpp
+++ devices/mbot-leds/src/main.cpp
@@ -16,9 +16,57 @@ void setup()
   leds.apply();
 }
 
+// Set this to true to use the HSV colorspace instead of RGB
+bool hsv_mode{true};
+
+void rgb_mode_loop();
+void hsv_mode_loop();
+
 void loop()
 {
-  // As an example, we will light each LED one by one in red and loop indefinitely.
+  if (hsv_mode)
+  {
+    // HSV mode: Change the hue of each LED independently
+    hsv_mode_loop();
+  }
+  else
+  {
+    // RGB mode: Set all LEDs to RED, one by one and loop.
+    rgb_mode_loop();
+  }
+}
+
+void hsv_mode_loop()
+{
+  // -> The index of the current LED
+  static uint8_t idx{0};
+
+  // -> Const saturation and value
+  static const uint8_t s = 100;
+  static const uint8_t v = 100;
+  static uint16_t h = random(361);
+
+  leds.set_color(idx, mbot::led_ring_t::hsv_t{h, s, v});
+  h+=random(30);
+  if (h > 360)
+  {
+    h = 0;
+  }
+
+  // -> Increment the index and loop if we reached the last LED.
+  ++idx;
+  if (idx == leds.led_count())
+  {
+    idx = 0;
+  }
+  // -> Send all data to the LED controller.
+  leds.apply();
+  // And wait a bit for the next loop.
+  delay(30);
+}
+
+void rgb_mode_loop()
+{
   // -> The index of the current LED
   static uint8_t idx{0};
   // -> start by resetting all LEDs to off.
@@ -33,7 +81,6 @@ void loop()
   }
   // -> Send all data to the LED controller.
   leds.apply();
-
   // And wait a bit for the next loop.
   delay(100);
 }
