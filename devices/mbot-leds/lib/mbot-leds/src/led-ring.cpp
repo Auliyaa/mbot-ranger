15f9fbc9d3ed1e8d96040f1c229a0388e896d4f3
Ludwig Fiolka <urushiraa@gmail.com>
2019-07-29 07:48:52 +0200

    [develop] mbot-leds: Added some doc and inlined some methods

--- devices/mbot-leds/lib/mbot-leds/src/led-ring.cpp
+++ devices/mbot-leds/lib/mbot-leds/src/led-ring.cpp
@@ -24,35 +24,15 @@ rgb_t led_ring_t::color_at(uint8_t idx) const
   return _device.getColorAt(idx);
 }
 
-bool led_ring_t::set_color(uint8_t idx, const rgb_t& rgb)
-{
-  return set_color(idx, rgb.r, rgb.g, rgb.b);
-}
-
 bool led_ring_t::set_color(uint8_t idx, uint8_t r, uint8_t g, uint8_t b)
 {
   return _device.setColor(idx+1, r, g, b);
-}
-
-bool led_ring_t::set_color(uint8_t idx, const hsv_t& hsv)
-{
-  return set_color(idx, hsv.to_rgb());
-}
-
-bool led_ring_t::set_color(const rgb_t& rgb)
-{
-  return set_color(rgb.r, rgb.g, rgb.b);
 }
 
 bool led_ring_t::set_color(uint8_t r, uint8_t g, uint8_t b)
 {
   // 0 means all LEDs
   return _device.setColor(0, r, g, b);
-}
-
-bool led_ring_t::set_color(const hsv_t& hsv)
-{
-  return set_color(hsv.to_rgb());
 }
 
 void led_ring_t::apply()
